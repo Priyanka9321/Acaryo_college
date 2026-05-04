2c5d0b2937884eb3797b7c0d1edc530ce9f18cdd
Priyanka Yadav <priyanyadav52004@gmail.com>
2026-05-04 23:47:50 +0530

    update hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,16 +4,25 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full min-h-screen flex items-center justify-center overflow-hidden">
-      {/* Animated background image */}
-      <div className="absolute inset-0 will-change-transform animate-ken-burns">
+    <section
+      className="
+        relative w-full overflow-hidden
+        h-[45vh] sm:h-[55vh] md:h-[65vh] lg:h-[80vh] xl:h-[90vh]
+        flex items-center justify-center
+      "
+    >
+      {/* Background image */}
+      <div className="absolute inset-0">
         <Image
           src="/assets/images/hero.jpeg"
           alt="Campus"
           fill
-          className="object-cover"
           priority
           sizes="100vw"
+          className="
+            object-cover object-center
+            lg:object-fill
+          "
         />
       </div>
     </section>
